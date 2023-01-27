298d37d64bdfff70ff3529d229852930c1d5c40f
spidey52 <satyamkumar5254@gmail.com>
2023-01-27 23:45:36 +0530

    executed

--- lib/parseBinanceSpotStream.ts
+++ lib/parseBinanceSpotStream.ts
@@ -1,3 +1,4 @@
+import TickerModel from "../models/ticker.models";
 import TradeModel from "../models/trades.models";
 import exchange from "./exchange.conn";
 
@@ -10,11 +11,18 @@ const findMinValueTrade = async (symbol: string, quantity: number) => {
  return trade;
 };
 
+async function test() {
+ const g = await TickerModel.findByIdAndUpdate("63c02612a6f85b8e75530d73", {
+  symbol: "BTCUSDT",
+  price: 1000,
+ });
+ console.log(g);
+}
+
+test();
+
 const parseBinanaceSpotStream = async (data: any) => {
- console.log(
-  "Binance Spot Stream Data",
-  console.log(new Date().toLocaleString())
- );
+ console.log("Binance Spot Stream Data", new Date().toString());
 
  const {
   e: eventType,
@@ -46,8 +54,35 @@ const parseBinanaceSpotStream = async (data: any) => {
     });
 
     // create new order
-    if (!updatedTrade) return;
-    exchange.createLimitBuyOrder(symbol, quantity, updatedTrade?.buyPrice);
+    if (!updatedTrade) {
+     console.log(
+      "No trade found",
+      symbol,
+      quantity,
+      updatedTrade,
+      "line number 59"
+     );
+     return;
+    }
+    console.log(
+     "New order starting",
+     symbol,
+     quantity,
+     updatedTrade?.buyPrice,
+     "line number 63"
+    );
+    await exchange.createLimitBuyOrder(
+     symbol,
+     quantity,
+     updatedTrade?.buyPrice
+    );
+    console.log(
+     "New order created",
+     symbol,
+     quantity,
+     updatedTrade?.buyPrice,
+     "line number 65"
+    );
    }
   }
  }
