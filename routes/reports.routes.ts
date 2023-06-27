6a85c9b5ff4d93f45b4b4e8a439537855b939f8e
spidey52 <satyamkumar5254@gmail.com>
2023-06-27 20:45:19 +0530

    implement future trade profit

--- routes/reports.routes.ts
+++ routes/reports.routes.ts
@@ -7,6 +7,11 @@ const router = Router();
 
 router.get("/", async (req, res) => {
  try {
+  const isFuture = req.query.future;
+  if (isFuture) {
+   const dailyReports = await getFutureDailyReports();
+   return res.status(200).json(dailyReports);
+  }
   const dailyReports = await getDailyReports();
   return res.status(200).json(dailyReports);
  } catch (error) {
@@ -30,6 +35,52 @@ router.get("/future/symbol", async (req, res) => {
 
 const getDailyReports = async () => {
  const reports = await TradeModel.aggregate([
+  {
+   $match: {
+    sellPrice: { $exists: true },
+   },
+  },
+  {
+   $addFields: {
+    sellTime: { $toDate: "$sellTime" },
+   },
+  },
+  {
+   $group: {
+    _id: {
+     updatedAt: { $dateToString: { format: "%Y-%m-%d", date: "$sellTime", timezone: "+05:30" } },
+    },
+    profit: {
+     $sum: {
+      $multiply: [{ $subtract: ["$sellPrice", "$buyPrice"] }, "$quantity"],
+     },
+    },
+   },
+  },
+  {
+   $sort: {
+    _id: 1,
+   },
+  },
+ ]);
+
+ let totalProfit = 0;
+ let count = 0;
+ reports.map((report) => {
+  count++;
+  totalProfit += report.profit;
+  const avgProfit = totalProfit / count;
+  report.avgProfit = avgProfit;
+  report.totalProfit = totalProfit;
+
+  return report;
+ });
+
+ return reports;
+};
+
+const getFutureDailyReports = async () => {
+ const reports = await FutureTradeModel.aggregate([
   {
    $match: {
     sellPrice: { $exists: true },
