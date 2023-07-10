d567dfac91bc5784965f73c7854e261c3975623d
spidey52 <satyamkumar5254@gmail.com>
2023-07-10 16:54:52 +0530

    type in trade sync

--- lib/trade.sync.ts
+++ lib/trade.sync.ts
@@ -46,33 +46,33 @@ export const cancelOrder = async (id: string, symbol: string) => {
  return order;
 };
 
-const tradeSync = async () => {
- const tickers = await TickerModel.find({});
- const symbols = tickers.map((ticker) => ticker.symbol);
- for (let i = 0; i < symbols.length; i++) {
-  const lastTrades = await TradeModel.find({ symbol: symbols[i] })
-   .sort({ createdAt: -1 })
-   .limit(1);
-  const lastTrade =
-   lastTrades.length === 0
-    ? Date.now() - 1000 * 60 * 60 * 24
-    : new Date(lastTrades[0].createdAt).getTime();
+// const tradeSync = async () => {
+//  const tickers = await TickerModel.find({});
+//  const symbols = tickers.map((ticker) => ticker.symbol);
+//  for (let i = 0; i < symbols.length; i++) {
+//   const lastTrades = await TradeModel.find({ symbol: symbols[i] })
+//    .sort({ createdAt: -1 })
+//    .limit(1);
+//   const lastTrade =
+//    lastTrades.length === 0
+//     ? Date.now() - 1000 * 60 * 60 * 24
+//     : new Date(lastTrades[0].createdAt).getTime();
 
-  const trades = (await getTrades(symbols[i]))
-   .filter((trade) => new Date(trade.timestamp).getTime() > lastTrade)
-   .filter((trade) => trade.side === "buy");
-  for (let j = 0; j < trades.length; j++) {
-   const trade = trades[j].info;
-   const newTradeBody = {
-    buyPrice: trade.price,
-    quantity: trade.qty,
-    symbol: trade.symbol,
-    user: "63beffd81c1312d53375a43f",
-   };
-   await TradeModel.create(newTradeBody);
-  }
- }
-};
+//   const trades = (await getTrades(symbols[i]))
+//    .filter((trade) => new Date(trade.timestamp).getTime() > lastTrade)
+//    .filter((trade) => trade.side === "buy");
+//   for (let j = 0; j < trades.length; j++) {
+//    const trade = trades[j].info;
+//    const newTradeBody = {
+//     buyPrice: trade.price,
+//     quantity: trade.qty,
+//     symbol: trade.symbol,
+//     user: "63beffd81c1312d53375a43f",
+//    };
+//    await TradeModel.create(newTradeBody);
+//   }
+//  }
+// };
 
 const autoTradeSync = async () => {
  let listenerKey = "";
